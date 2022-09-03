56bba2054439aaba7536b3d20918882165e0b2d9
Andrey Sapozhnikov <andreytapok@yandex.ru>
2022-09-03 22:34:09 +0800

    feat(profile): раскидал блоки по БЭМ
    раскидал все блоки по БЭМ, остался только попап

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-//Выбираем элементы из профайла
-let profile = document.querySelector('.profile__info');
+const overlay = document.querySelector('.form__overlay');
+let form = overlay.querySelector('.profile__form');
+let formName = overlay.querySelector('.profile__form-name');
+let formJob = overlay.querySelector('.profile__form-job');
+const closeButton = overlay.querySelector('.profile__form-close-button');
+
+const profile = document.querySelector('.profile');
+const profileEdit = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__job');
-let profileEditButton = profile.querySelector('.profile__edit-button');
 
-//выбираем элементы из формы
-// TODO переделать выбор селекторов
-let formOverlay = document.querySelector('.form__overlay');
-let form = document.querySelector('.profile__form');
-let formName = form.querySelector('#name');
-let formJob = form.querySelector('#job');
-let formSaveButton = form.querySelector('.profile__form-save-button');
-let formCloseButton = form.querySelector('.profile__form-close-button');
+//слушатель на открытие формы
+profileEdit.addEventListener('click', () => {
+  overlay.setAttribute('style', 'display: block');
+  formName['value'] = profileName.textContent;
+  formJob['value'] = profileJob.textContent;
+})
 
-//функция на передачу информации из профайла в форму + отображение формы
-function profileEditHandler(evt) {
-  formOverlay.setAttribute('style', 'display: block');
-  formName.setAttribute('value', `${profileName.textContent}`);
-  formJob.setAttribute('value', `${profileJob.textContent}`);
-}
+//слушатель на закрытие формы
+closeButton.addEventListener('click', () => {
+  overlay.setAttribute('style', 'display: none');
+})
 
-function closeForm(evt) {
-  evt.preventDefault();
-  formOverlay.setAttribute('style', 'display: none');
-}
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  profileName.textContent = formName.value;
-  profileJob.textContent = formJob.value;
-  closeForm(evt);
-}
-
-profileEditButton.addEventListener('click', profileEditHandler);
-formSaveButton.addEventListener('click', formSubmitHandler);
-formCloseButton.addEventListener('click', closeForm);
+//слушатель на форму
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  profileName.textContent = formName['value'];
+  profileJob.textContent = formJob['value'];
+  overlay.setAttribute('style', 'display: none');
+} );
 
 //на странице выбираем кнопки лайков
-let elements = document.querySelector('.elements__grid');
-let element = elements.querySelectorAll('.element');
+const elements = document.querySelector('.elements__grid');
 let likeButtons = elements.querySelectorAll('.element__button');
 
 //для каждой кнопки лайка при нажатии добавляем/убираем класс element__button_active
