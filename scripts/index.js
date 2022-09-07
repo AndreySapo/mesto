baf4b6ea516c8dad5b278645c877bf8879f13f20
Andrey Sapo <andreytapok@yandex.ru>
2022-09-07 10:35:27 +0800

    удаление комментариев из js

--- scripts/index.js
+++ scripts/index.js
@@ -19,19 +19,12 @@ let popupInputJob = popup.querySelector('#job');
 const popupCloseButton = popup.querySelector('.popup__button-close');
 
 function openPopup() {
-// Это круто, что вы знаете про тоггл. Но по заданию при открытии попапа данные должны сразу заноситься в форму. 
-// Причем, если только при открытии, то значит при использовании тоггла необходимо проверить, содержит ли попап класс открытия.
-// Для этого необходимо использовать условие и воспользоваться методом classList.contains. 
-// Можно лучше: Лучше функцию с classList.toggle разделить на две разные функции: открытия попапа и закрытия.
-// Так ими легче будет управлять и поддерживать в дальнейшем
   popupInputName.value = profileName.textContent;
   popupInputJob.value = profileJob.textContent;
   popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-// Если оставите функцию с тогглом, то функция закрытия будет не нужна, так как тоггл и добавляет, и удаляет класс (по принципу выключателя)
-// Подробнее http://code.mu/ru/javascript/manual/dom/classList/toggle/
   popup.classList.remove('popup_opened');
 }
 
