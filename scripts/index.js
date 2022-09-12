102aac7b1748184c93ddd580abbb4cd04303a0e2
Andrey Sapo <andreytapok@yandex.ru>
2022-09-12 20:40:48 +0800

    template cards
    1. изменен метод добавления изначальных карточек на темплейт
    2. изменены сами изначальные карточки. лежат в массиве initialCards
    3. немного подправлен css для корректного отображения лайка(уезжала вправо)
    работа за 12.09
    
    1. добавлена форма добавления постов
    2. при добавлении поста карточка встает на первое место.
    3. сделаны лайки, привязаны к карточке при создании
    4. сделана кнопка удалить пост, привязана к карточке при создании
    
    TODO
    1. открытие попапа с картинкой.
    как я понял - оверлей>картинка+подпись+кнопка. непонятно, как отловить клик, делать ссылку на картинку в гриде? или кнопку?
    2. Плавное открытие попапов - посмотреть на ссылку в практикуме, там вроде не сложно

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,157 @@
-// Код для формы
-// 1. Получаем необходимые объекты из DOM
-// 2. создаем 3 функции.
-//      открытие попапа с записью в инпуты информации из профайла
-//      закрытие попапа
-//      сохранение информации из инпута в профайл + закрытие попапа
-// 3. добавляем слушатели на нужные кнопки из первого пункта.
-// P.S. сохранение идет по submit, который вешается на форму
+// место куда вставлять темплейт с карточками + массив для объектов карточек
+const cardsContainer = document.querySelector('.elements__grid');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
+// переменные для profile
 const profile = document.querySelector('.profile');
-let profileName = profile.querySelector('.profile__name');
-let profileJob = profile.querySelector('.profile__job');
+const profileName = profile.querySelector('.profile__name');
+const profileJob = profile.querySelector('.profile__job');
 const profileEditButton = profile.querySelector('.profile__edit-button');
+const profileAddButton = profile.querySelector('.profile__add-button');
 
-const popup = document.querySelector('.popup');
-const popupForm = popup.querySelector('.popup__form');
-let popupInputName = popup.querySelector('#name');
-let popupInputJob = popup.querySelector('#job');
-const popupCloseButton = popup.querySelector('.popup__button-close');
+// переменные для profilePopup
+const profilePopup = document.querySelector('.profile-popup');
+const profilePopupForm = profilePopup.querySelector('.popup__form');
+const profilePopupName = profilePopup.querySelector('#name');
+const profilePopupJob = profilePopup.querySelector('#job');
+const profilePopupCloseButton = profilePopup.querySelector('.popup__button-close');
 
-function openPopup() {
-  popupInputName.value = profileName.textContent;
-  popupInputJob.value = profileJob.textContent;
+// переменные для new-post-popup
+const newPostPopup = document.querySelector('.new-post-popup');
+const newPostPopupForm = newPostPopup.querySelector('.popup__form');
+const newPostPopupPlace = newPostPopup.querySelector('#place');
+const newPostPopupPicture = newPostPopup.querySelector('#picture');
+const newPostPopupCloseButton = newPostPopup.querySelector('.popup__button-close');
+
+
+function createCard(card) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardTemplateClone = cardTemplate.cloneNode(true);
+
+  const cardImage = cardTemplateClone.querySelector('.element__image');
+  const cardTitle = cardTemplateClone.querySelector('.element__title');
+
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+
+  const cardLikeButton = cardTemplateClone.querySelector('.element__button-like');
+  const cardTrashButton = cardTemplateClone.querySelector('.element__button-trash');
+
+  function toggleLike(event) {
+    event.target.classList.toggle('element__button-like_active');
+  }
+
+  function deleteElement() {
+    const listItem = cardTrashButton.closest('.element');
+    listItem.remove();
+  }
+
+  cardLikeButton.addEventListener('click', toggleLike);
+  cardTrashButton.addEventListener('click', deleteElement);
+
+  return cardTemplateClone;
+}
+
+function createFirstCards(card) {
+  cardsContainer.append(createCard(card));
+}
+
+function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove('popup_opened');
+}
+
+function defaultInputValue(popup) {
+  arrayOfInputs = popup.querySelectorAll('.popup__input');
+  arrayOfInputs.forEach(element => { element.value = '' });
+}
+
+// частные функции
+function sendToInput() {
+  profilePopupName.value = profileName.textContent;
+  profilePopupJob.value = profileJob.textContent;
 }
 
 function saveToProfile(event) {
   event.preventDefault();
-  profileName.textContent = popupInputName.value;
-  profileJob.textContent = popupInputJob.value;
+  profileName.textContent = profilePopupName.value;
+  profileJob.textContent = profilePopupJob.value;
 }
 
-function saveAndClose(event) {
+function addCard(event) {
+  event.preventDefault();
+
+  const card = {
+    name: newPostPopupPlace.value,
+    link: newPostPopupPicture.value
+  }
+
+  cardsContainer.prepend(createCard(card));
+}
+
+// вызовы функций
+initialCards.forEach(createFirstCards);
+
+profileEditButton.addEventListener('click', () => {
+  sendToInput();
+  openPopup(profilePopup);
+});
+
+profilePopupForm.addEventListener('submit', () => {
   saveToProfile(event);
-  closePopup();
-}
+  closePopup(profilePopup);
+  defaultInputValue(profilePopup);
+});
 
-profileEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', saveAndClose);
+profilePopupCloseButton.addEventListener('click', () => {
+  closePopup(profilePopup);
+  defaultInputValue(profilePopup);
+});
 
-// код для лайков
-// 1. Получаем массив кнопок из родительского для них элемента
-// 2. для каждого элемента из массива кнопок добавляем слушатель с нужной функцией
-// 3. нужная функция переключает нужный класс для кнопки
+profileAddButton.addEventListener('click', () => {
+  openPopup(newPostPopup);
+});
 
-// function toggleLike(event) {
-//   event.target.classList.toggle('element__button_active');
-// }
+newPostPopupCloseButton.addEventListener('click', () => {
+  closePopup(newPostPopup);
+  defaultInputValue(newPostPopup);
+});
 
-// const elements = document.querySelector('.elements__grid');
-// let likeButtonsArray = elements.querySelectorAll('.element__button');
+// тестовая карточка
+// Ангарск
+// https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Angarsk_car_Volga_GAZ-21_%2825720495842%29.jpg/1920px-Angarsk_car_Volga_GAZ-21_%2825720495842%29.jpg
 
-// likeButtonsArray.forEach(button => button.addEventListener('click', toggleLike));
+newPostPopupForm.addEventListener('submit', () => {
+  addCard(event);
+  closePopup(newPostPopup);
+  defaultInputValue(newPostPopup);
+})
