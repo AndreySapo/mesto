3502e165422469e1d77f281d74ea2550c2217092
Andrey Sapo <andreytapok@yandex.ru>
2022-09-13 23:35:59 +0800

    работа за 13.09
    1. сделал открытие попапа с картинкой
    2. распихал всё по БЭМ
    3. сделал комменты к js
    
    TODO
    Плавное открытие и закрытие попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,5 @@
-// место куда вставлять темплейт с карточками + массив для объектов карточек
+// место куда вставлять темплейт с карточками
 const cardsContainer = document.querySelector('.elements__grid');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 // переменные для profile
 const profile = document.querySelector('.profile');
@@ -48,7 +22,29 @@ const newPostPopupPlace = newPostPopup.querySelector('#place');
 const newPostPopupPicture = newPostPopup.querySelector('#picture');
 const newPostPopupCloseButton = newPostPopup.querySelector('.popup__button-close');
 
+// функция открытия ЛЮБОГО попапа
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
 
+// функция закрытия ЛЮБОГО попапа
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// функция обнуления инпутов в ЛЮБОМ попапе
+function defaultInputValue(popup) {
+  arrayOfInputs = popup.querySelectorAll('.popup__input');
+  arrayOfInputs.forEach(element => { element.value = '' });
+}
+
+// функция создания карточки
+// принимает на вход объект карточки
+// берем ноду темплейта карточки, копируем её, нужным элементам
+// присваиваем необходимые для отображения значения
+// создаем кнопки лайка, удаления и зума
+// добавляем кнопкам слушатели
+// обязательно возвращаем html-код карточки
 function createCard(card) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardTemplateClone = cardTemplate.cloneNode(true);
@@ -62,6 +58,7 @@ function createCard(card) {
 
   const cardLikeButton = cardTemplateClone.querySelector('.element__button-like');
   const cardTrashButton = cardTemplateClone.querySelector('.element__button-trash');
+  const cardZoomButton = cardTemplateClone.querySelector('.element__button-zoom');
 
   function toggleLike(event) {
     event.target.classList.toggle('element__button-like_active');
@@ -75,38 +72,61 @@ function createCard(card) {
   cardLikeButton.addEventListener('click', toggleLike);
   cardTrashButton.addEventListener('click', deleteElement);
 
+  const popupZoom = document.querySelector('.img-zoom');
+  const imagePopupZoom = popupZoom.querySelector('.img-zoom__img');
+  const captionPopupZoom = popupZoom.querySelector('.img-zoom__caption');
+  const buttonClosePopupZoom = popupZoom.querySelector('.img-zoom__button-close');
+
+
+  function copyToPopupZoom() {
+    imagePopupZoom.src = card.link;
+    imagePopupZoom.alt = card.name;
+    captionPopupZoom.textContent = card.name;
+  }
+
+  cardZoomButton.addEventListener('click', () => {
+    copyToPopupZoom();
+    openPopup(popupZoom);
+  });
+
+  function defaultZoomValue() {
+    imagePopupZoom.src = '';
+    imagePopupZoom.alt = '';
+    captionPopupZoom.textContent = '';
+  }
+
+  buttonClosePopupZoom.addEventListener('click', () => {
+    closePopup(popupZoom);
+    defaultZoomValue();
+  });
+
   return cardTemplateClone;
 }
 
+// функция создания первых карточек
+// получает карточку и добавляет полученную из функции createCard карточку
+// в конец списка карточек
 function createFirstCards(card) {
   cardsContainer.append(createCard(card));
 }
 
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
-
-function defaultInputValue(popup) {
-  arrayOfInputs = popup.querySelectorAll('.popup__input');
-  arrayOfInputs.forEach(element => { element.value = '' });
-}
-
-// частные функции
+// функция передачи контента из профайла в попап
 function sendToInput() {
   profilePopupName.value = profileName.textContent;
   profilePopupJob.value = profileJob.textContent;
 }
 
+// функция сохранения контента из попапа в профайл
 function saveToProfile(event) {
   event.preventDefault();
   profileName.textContent = profilePopupName.value;
   profileJob.textContent = profilePopupJob.value;
 }
 
+// функция добавления новой карточки
+// генерируем объект карточки из полученных значений от пользователя
+// передаем карточку в функцию создания карточки и добавляем её
+// в начало списка карточек
 function addCard(event) {
   event.preventDefault();
 
@@ -119,28 +139,38 @@ function addCard(event) {
 }
 
 // вызовы функций
+// для каждой карточки из списка начальных карточек выполнить функцию создания карточки одна за одной
 initialCards.forEach(createFirstCards);
 
+// слушатель кнопки редактирования профиля
+// сначала передаем данные в инпуты, потом открываем попап
 profileEditButton.addEventListener('click', () => {
   sendToInput();
   openPopup(profilePopup);
 });
 
+// слушатель кнопки сохранения данных из попапа в профайл
+// сохраняем=>закрываем=>очищаем инпуты
 profilePopupForm.addEventListener('submit', () => {
   saveToProfile(event);
   closePopup(profilePopup);
   defaultInputValue(profilePopup);
 });
 
+// слушатель кнопки закрытия попапа
+// закрываем=>очищаем инпуты
 profilePopupCloseButton.addEventListener('click', () => {
   closePopup(profilePopup);
   defaultInputValue(profilePopup);
 });
 
+// слушатель кнопки открытия попапа профайла
 profileAddButton.addEventListener('click', () => {
   openPopup(newPostPopup);
 });
 
+// слушатель кнопки закрытия попапа нового поста
+// закрываем попап=>очищаем инпуты
 newPostPopupCloseButton.addEventListener('click', () => {
   closePopup(newPostPopup);
   defaultInputValue(newPostPopup);
@@ -149,7 +179,11 @@ newPostPopupCloseButton.addEventListener('click', () => {
 // тестовая карточка
 // Ангарск
 // https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Angarsk_car_Volga_GAZ-21_%2825720495842%29.jpg/1920px-Angarsk_car_Volga_GAZ-21_%2825720495842%29.jpg
+// Карачаево-Черкессия
+// ./images/Image.jpg
 
+// слушатель кнопки создания нового поста
+// добавляем карточку=>закрываем попап=>очищаем инпуты
 newPostPopupForm.addEventListener('submit', () => {
   addCard(event);
   closePopup(newPostPopup);
