86609212eb9b96adcc352b0aa44dfd78ff543708
Andrey Sapo <andreytapok@yandex.ru>
2022-10-25 10:40:06 +0800

    fix 2
    Функцию необходимо разделить на две отдельных, чтобы была возможность переиспользовать их по отдельности и чтобы не осуществлять дополнительной нагрузочной проверки, так как слушатели закрытия кликом по оверлею и нажатием на эскейп устанавливаются на разные события

--- scripts/index.js
+++ scripts/index.js
@@ -34,18 +34,20 @@ const newPostPopupSaveButton = newPostPopup.querySelector('.popup__button-save')
 // функция закрытия ЛЮБОГО попапа
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closeByEscapeOrClick);
-  document.removeEventListener('click', closeByEscapeOrClick);
+  document.removeEventListener('keydown', closeByEscape);
+  document.removeEventListener('click', closeByClick);
 }
 
-// функция слушателей кнопки esc и щелчка по оверлею
-function closeByEscapeOrClick(evt) {
+// функции слушателей кнопки esc и щелчка по оверлею
+function closeByEscape(evt) {
   // если esc, то находим открытый попап и закрываем его
   if (evt.key === 'Escape') {
     const currentPopup = document.querySelector('.popup_opened');
     closePopup(currentPopup);
   }
+}
 
+function closeByClick(evt) {
   // если клик по оверлею - то находим открытый попап и закрываем его
   if (evt.target.classList.contains('popup')) {
     const currentPopup = document.querySelector('.popup_opened');
@@ -57,8 +59,8 @@ function closeByEscapeOrClick(evt) {
 function openPopup(popup) {
   popup.classList.add('popup_opened');
   // добавляем два слушателя, на клик и на клавишу
-  document.addEventListener('keydown', closeByEscapeOrClick);
-  document.addEventListener('click', closeByEscapeOrClick);
+  document.addEventListener('keydown', closeByEscape);
+  document.addEventListener('click', closeByClick);
 }
 
 export { openPopup }
