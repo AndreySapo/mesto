ee24d260cbde6d39da4be9a494337669613ec7b8
Andrey Sapo <andreytapok@yandex.ru>
2022-10-27 00:57:45 +0800

    Card.js -> this.

--- scripts/Card.js
+++ scripts/Card.js
@@ -3,37 +3,37 @@ import {openPopup} from './utils.js';
 class Card {
   constructor(data, template) { //в конструктор передаем объект данных, где содержится имя и ссылка на картинку. и ещё передаем название шаблона
     // поиск и копирование шаблона 
-    const _cardTemplate = document.querySelector(template).content;
-    const cardTemplateClone = _cardTemplate.cloneNode(true);
+    this._cardTemplate = document.querySelector(template).content;
+    this.cardTemplateClone = this._cardTemplate.cloneNode(true);
 
     // переменные для img-zoom
-    this._imgZoomPopup = document.querySelector('.img-zoom');
-    this._imageImgZoomPopup = this._imgZoomPopup.querySelector('.img-zoom__img');
-    this._captionImgZoomPopup = this._imgZoomPopup.querySelector('.img-zoom__caption');
+    this.this._imgZoomPopup = document.querySelector('.img-zoom');
+    this.this._imageImgZoomPopup = this.this._imgZoomPopup.querySelector('.img-zoom__img');
+    this.this._captionImgZoomPopup = this.this._imgZoomPopup.querySelector('.img-zoom__caption');
 
     // из шаблона выбираем картинку и подпись 
-    const _cardImage = cardTemplateClone.querySelector('.element__image');
-    const _cardTitle = cardTemplateClone.querySelector('.element__title');
+    this._cardImage = this.cardTemplateClone.querySelector('.element__image');
+    this._cardTitle = this.cardTemplateClone.querySelector('.element__title');
 
     // выбранным картинкам и подписям присваиваем значения из полученной карточки 
-    _cardImage.src = data.link;
-    _cardImage.alt = data.name;
-    _cardTitle.textContent = data.name;
+    this._cardImage.src = data.link;
+    this._cardImage.alt = data.name;
+    this._cardTitle.textContent = data.name;
 
     // из шаблона выбираем кнопки
-    const _cardLikeButton = cardTemplateClone.querySelector('.element__button-like');
-    const _cardTrashButton = cardTemplateClone.querySelector('.element__button-trash');
-    const _cardZoomButton = cardTemplateClone.querySelector('.element__button-zoom');
+    this._cardLikeButton = this.cardTemplateClone.querySelector('.element__button-like');
+    this._cardTrashButton = this.cardTemplateClone.querySelector('.element__button-trash');
+    this._cardZoomButton = this.cardTemplateClone.querySelector('.element__button-zoom');
 
     // слушатели кнопок
-    _cardLikeButton.addEventListener('click', this._toggleLike);
-    _cardTrashButton.addEventListener('click', this._deleteElement);
-    _cardZoomButton.addEventListener('click', () => {
+    this._cardLikeButton.addEventListener('click', this._toggleLike);
+    this._cardTrashButton.addEventListener('click', this._deleteElement);
+    this._cardZoomButton.addEventListener('click', () => {
       this._copyToPopupZoom(data);
-      openPopup(this._imgZoomPopup);
+      openPopup(this.this._imgZoomPopup);
     });
 
-    return cardTemplateClone
+    return this.cardTemplateClone
   }
 
   // функция изменения состояния кнопки лайка
@@ -49,9 +49,9 @@ class Card {
 
   // присваиваем нужные значения в попап картинки
   _copyToPopupZoom(data) {
-    this._imageImgZoomPopup.src = data.link;
-    this._imageImgZoomPopup.alt = data.name;
-    this._captionImgZoomPopup.textContent = data.name;
+    this.this._imageImgZoomPopup.src = data.link;
+    this.this._imageImgZoomPopup.alt = data.name;
+    this.this._captionImgZoomPopup.textContent = data.name;
   }
 }
 
