24fb0a156e337a212b9bdcbcb257d69980b96cc3
Andrey Sapo <andreytapok@yandex.ru>
2022-10-27 12:27:32 +0800

    fix 7
    Работая с карточкой, мы работаем с ее конкретным экземпляром класса, поэтому необходимо сразу удалять элемент this._element.remove(). А после удаления элемент лучше зануллить.
    Для этого сам элемент карточки мы должны поместить в поле класса this._element при его создании

--- scripts/Card.js
+++ scripts/Card.js
@@ -4,7 +4,9 @@ class Card {
   constructor(data, template) { //в конструктор передаем объект данных, где содержится имя и ссылка на картинку. и ещё передаем название шаблона
     // поиск и копирование шаблона 
     this._cardTemplate = document.querySelector(template).content;
-    this.cardTemplateClone = this._cardTemplate.cloneNode(true);
+    // console.log(this._cardTemplate)
+    this._cardTemplateClone = this._cardTemplate.cloneNode(true);
+    this._element = this._cardTemplateClone.querySelector('.element');
 
     // переменные для img-zoom
     this._imgZoomPopup = document.querySelector('.img-zoom');
@@ -12,8 +14,8 @@ class Card {
     this._captionImgZoomPopup = this._imgZoomPopup.querySelector('.img-zoom__caption');
 
     // из шаблона выбираем картинку и подпись 
-    this._cardImage = this.cardTemplateClone.querySelector('.element__image');
-    this._cardTitle = this.cardTemplateClone.querySelector('.element__title');
+    this._cardImage = this._element.querySelector('.element__image');
+    this._cardTitle = this._element.querySelector('.element__title');
 
     // выбранным картинкам и подписям присваиваем значения из полученной карточки 
     this._cardImage.src = data.link;
@@ -21,19 +23,21 @@ class Card {
     this._cardTitle.textContent = data.name;
 
     // из шаблона выбираем кнопки
-    this._cardLikeButton = this.cardTemplateClone.querySelector('.element__button-like');
-    this._cardTrashButton = this.cardTemplateClone.querySelector('.element__button-trash');
-    this._cardZoomButton = this.cardTemplateClone.querySelector('.element__button-zoom');
+    this._cardLikeButton = this._element.querySelector('.element__button-like');
+    this._cardTrashButton = this._element.querySelector('.element__button-trash');
+    this._cardZoomButton = this._element.querySelector('.element__button-zoom');
 
     // слушатели кнопок
     this._cardLikeButton.addEventListener('click', this._toggleLike);
-    this._cardTrashButton.addEventListener('click', this._deleteElement);
+    this._cardTrashButton.addEventListener('click', () => {
+      this._deleteElement()
+    });
     this._cardZoomButton.addEventListener('click', () => {
       this._copyToPopupZoom(data);
-      openPopup(this.this._imgZoomPopup);
+      openPopup(this._imgZoomPopup);
     });
 
-    return this.cardTemplateClone
+    return this._element
   }
 
   // функция изменения состояния кнопки лайка
@@ -42,17 +46,15 @@ class Card {
   }
 
   // функция удаления карточки из списка карточек
-  _deleteElement = function (event) {
-    const listItem = event.target.parentElement;
-    listItem.remove();
-    // console.log(listItem)
+  _deleteElement = function () {
+    this._element.remove();
   }
 
   // присваиваем нужные значения в попап картинки
   _copyToPopupZoom(data) {
-    this.this._imageImgZoomPopup.src = data.link;
-    this.this._imageImgZoomPopup.alt = data.name;
-    this.this._captionImgZoomPopup.textContent = data.name;
+    this._imageImgZoomPopup.src = data.link;
+    this._imageImgZoomPopup.alt = data.name;
+    this._captionImgZoomPopup.textContent = data.name;
   }
 }
 
